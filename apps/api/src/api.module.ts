955c67736b902e1072fd2f177c4f5ec8497cf63c
Rifaudeen <rifaudeen.ahamad@mywhoosh.com>
2022-09-09 10:33:12 +0400

    renamed config key for port

--- apps/api/src/api.module.ts
+++ apps/api/src/api.module.ts
@@ -2,7 +2,7 @@ import { AuthLibraryModule } from '@app/auth-library';
 import configuration from '@app/common/config/configuration';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import apiConfiguration from '../config/api.configuration';
+import apiConfiguration from './config/api.configuration';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(ApiModule);
   app.setGlobalPrefix('api');
   const config = app.get(ConfigService);
-  const port = config.get<number>('api_port');
+  const port = config.get<number>('api_server_port');
   await app.listen(port);
   console.log(`🚀 API server ready at http://localhost:${port}`);
 }
